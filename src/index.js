3e684048c85da724445272c59aefed512478bf7c
Itachi-MacOS <warrior.006@inbox.ru>
2023-04-05 23:30:06 +0600

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('./styles/main.css')
 
 
 window.addEventListener('load', (event) => {
+  // Вырубаем zoom in/out на айосах
   if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
     window.document.addEventListener('touchmove', function(e) {
       if(e.scale !== 1) {
@@ -10,6 +11,9 @@ window.addEventListener('load', (event) => {
     }, {passive: false});
   }
   formHandlerer()
+  menuLogic()
+  localeHandler()
+  setScrollBehavior()
 });
 
 
@@ -18,44 +22,127 @@ function formHandlerer () {
   var form = document.querySelector('#landing-form')
   var name = document.querySelector('#input-name')
   var email = document.querySelector('#input-email')
-
-  form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    
-    if (!name.value || !email.value) {
-      triggerValidationError()
-      return
-    }
-
-    var params = {
-      name: name.value,
-      email: email.value
-    }
-
-    fetch('https://smmart.pro/api/test-users/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(params)
-    }).then(data => {
-      submitSucceed()
-      name.value = ''
-      email.value = ''
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault()
+      
+      if (!name.value || !email.value) {
+        triggerValidationError()
+        return
+      }
+  
+      var params = {
+        name: name.value,
+        email: email.value
+      }
+  
+      fetch('https://api.smm.art/api/test-users/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+      }).then(data => {
+        submitSucceed()
+        name.value = ''
+        email.value = ''
+      })
     })
-  })
+  }
 }
 
 function triggerValidationError () {
-  const errorText = document.querySelector('.form__error')
+  var errorText = document.querySelector('.form__error')
   errorText.style.opacity = 1
 }
 
 function submitSucceed () {
-  const successMessageEl = document.querySelector('.form__success')
-  const formWrapper = document.querySelector('.form__inputs-wrapper')
+  var successMessageEl = document.querySelector('.form__success')
+  var formWrapper = document.querySelector('.form__inputs-wrapper')
   successMessageEl.style.display = 'block'
   formWrapper.style.display = 'none'
 
+}
+
+function menuLogic () {
+  var burger = document.querySelector('#burger')
+  var closemenu = document.querySelector('#close-menu')
+  var menu = document.querySelector('#mobile-menu')
+
+  if (closemenu && burger) {
+    closemenu.addEventListener('click', function () {
+      menu.style.display = 'none'
+    })
+  
+    burger.addEventListener('click', function () {
+      menu.style.display = 'block'
+    })
+  }
+}
+
+
+function localeHandler () {
+  var activeLang = location.pathname.startsWith('/en') ? 'en' : 'ru'
+  var langs = document.querySelectorAll('.lang-switcher__item')
+
+  if (langs.length) {
+    langs.forEach(function (x) {
+      x.addEventListener('click', function (e) {
+        var pathname = window.location.pathname
+        if (x.dataset.lang === 'en' && activeLang !== 'en') {
+          localStorage.setItem('activeLang', 'en')
+          window.location.href = pathname === '/' ? '/en' : '/en' + pathname
+        }
+        if (x.dataset.lang === 'ru' && activeLang !== 'ru') {
+          localStorage.setItem('activeLang', 'ru')
+          window.location.href = pathname.startsWith('/en/') ? pathname.split('/en').join('') : pathname.split('/en').join('/')
+        }
+      })
+    })
+  }
+
+  // Article page back btn handle depend on value in localStorage
+  var storageLang = localStorage.getItem('activeLang')
+  var locale =  storageLang ? storageLang : 'ru' 
+
+  var articleBackBtn = document.querySelector('.article-back-btn')
+  if (articleBackBtn) {
+    articleBackBtn.addEventListener('click', function(e) {
+      if (locale === 'ru') {
+        return
+      } else {
+        e.preventDefault()
+        window.location.href = '/en/posts.html'
+      }
+    })
+  }
+}
+
+function setScrollBehavior () {
+  function clickHandler(e) {
+    e.preventDefault();
+    var href = this.getAttribute("href");
+    var offsetTop = getOffsetTop(document.querySelector(href))
+    window.scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
+
+  var links = document.querySelectorAll('.app-menu__item--smooth a')
+
+  if (links.length) {
+    links.forEach(function (x) {
+      x.addEventListener('click', clickHandler)
+    })
+  }
+}
+var getOffsetTop = function (element) {
+  var offsetTop = 0;
+  while(element) {
+    offsetTop += element.offsetTop;
+    element = element.offsetParent;
+  }
+  return offsetTop;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,22 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { readdirSync } = require('fs');
+
+
+const htmlPagesNames = []
+const posts = readdirSync('./src/posts')
+posts.forEach(x => {
+  htmlPagesNames.push(x.split('.html')[0])
+})
+
+const multipleHtmlPlugins = htmlPagesNames.map(name => {
+  return new HtmlWebpackPlugin({
+    template: `./src/posts/${name}.html`,
+    filename: `${name}/index.html`
+  })
+})
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -15,10 +31,23 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    // new HtmlWebpackPlugin({
-    //   filename: 'ru-RU/index.html',
-    //   template: './src/ru-RU/index.html',
-    // }),
+    new HtmlWebpackPlugin({
+      template: './src/error.html',
+      filename: 'error.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/posts.html',
+      filename: 'posts.html'
+    }),
+    ...multipleHtmlPlugins,
+    new HtmlWebpackPlugin({
+      template: './src/en/index.html',
+      filename: 'en/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/en/posts.html',
+      filename: 'en/posts.html',
+    }),
     // new HtmlWebpackPlugin({
     //   filename: 'en-EN/index.html',
     //   template: './src/en-EN/index.html',
@@ -29,41 +58,53 @@ module.exports = {
     // }),
     new CopyWebpackPlugin({
       patterns: [
-        { 
+        {
           from: 'src/assets/*',
           to({ context, absoluteFilename }) {
             return "assets/[name][ext]";
-          }, 
+          },
         },
-        // { 
-        //   from: 'src/ru-RU/*',
+        {
+          from: 'src/images/*',
+          to({ context, absoluteFilename }) {
+            return "images/[name][ext]";
+          },
+        },
+        {
+          from: 'src/privacy/*',
+          to({ context, absoluteFilename }) {
+            return "privacy/[name][ext]";
+          },
+        },
+        {
+          from: 'src/root/*',
+          to({ context, absoluteFilename }) {
+            return "[name][ext]";
+          },
+        },
+        // {
+        //   from: 'src/posts/*',
         //   to({ context, absoluteFilename }) {
-        //     return "ru-RU/[name][ext]";
-        //   }, 
+        //     return "[name]/index[ext]";
+        //   },
         // },
-        // { 
+        // {
         //   from: 'src/en-EN/*',
         //   to({ context, absoluteFilename }) {
         //     return "ru-RU/[name][ext]";
-        //   }, 
+        //   },
         // },
-        // { 
+        // {
         //   from: 'src/kk-KZ/*',
         //   to({ context, absoluteFilename }) {
         //     return "kk-KZ/[name][ext]";
-        //   }, 
+        //   },
         // },
-        // { 
-        //   from: 'src/root/*',
-        //   to({ context, absoluteFilename }) {
-        //     return "[name][ext]";
-        //   }, 
-        // },
-        // { 
+        // {
         //   from: 'src/root/.github/workflows/*',
         //   to({ context, absoluteFilename }) {
         //     return ".github/workflows/[name][ext]";
-        //   }, 
+        //   },
         // }
       ]
     }),
@@ -91,7 +132,8 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader'],
+        include: path.resolve(__dirname, 'src'),
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(ttf|woff|woff2|eot)$/,
